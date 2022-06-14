9a799c607c75d968026149560d8e8b97cf909a03
Malins062 <6328673@gmail.com>
2022-06-14 12:46:36 +0300

    Работа над заданием 1 метод list

--- Chapter_1/Task_3/task_1.js
+++ Chapter_1/Task_3/task_1.js
@@ -1,4 +1,4 @@
-class Goods {
+class Good {
     /*
     Класс для хранения данных о товаре со свойствами:
         id            код товара
@@ -20,10 +20,7 @@ class Goods {
         this.available = available;
     }
 
-    /**
-     * @param {boolean} value
-     */
-    set available (value) {
+    set setAvailable (value) {
         this.available = value
     }
 }
@@ -40,10 +37,104 @@ class GoodList {
         add()          добавление товара в каталог
         remove(id)     удаление товара из каталога по его id
     */
+    
+    // #goods = [];
+    #goods;
 
     constructor (filter, sortPrice, sortDir) {
+        this.#goods = [];
         this.filter = filter;
         this.sortPrice = sortPrice;
         this.sortDir = sortDir;
     }
+
+    get list() {
+        if (this.sortPrice) {
+            return this.#goods.sort((prev, next) => prev.price - prev.next ? this.sortDir : !this.sortDir);
+        }
+    }
+
+    add (good) {
+        this.#goods.push(good);
+    }
+
+    remove (id) {
+        const _index = this.#goods.findIndex(g => g.id === id)
+        if (_index != undefined) {
+            this.#goods.splice(_index, 1);
+        }
+        return _index;
+    }
 }
+
+class BaketGood extends Good {
+    /*
+    BasketGood - класс дочерний от Good, для хранения данных о товаре в корзине с дополнительным свойством:
+        amount      количество товара в корзине
+    */
+
+    constructor (good) {
+        super(good)
+        this.amount = amount;
+    }
+}
+
+class Basket {
+    /*
+    Basket - класс для хранения данных о корзине товаров со свойствами:
+        goods       массив объектов класса BasketGood для хранения данных о товарах в корзине
+
+        get totalAmount()  возвращает общую стоимость товаров в корзине
+        get totalSum()     возвращает общее количество товаров в корзине
+
+        add(good, amount)    Добавляет товар в корзину, если товар уже есть увеличивает количество
+        remove(good, amount) Уменьшает количество товара в корзине, если количество становится равным нулю, товар удаляется
+        clear()              Очищает содержимое корзины
+        removeUnavailable()  Удаляет из корзины товары, имеющие признак available === false (использовать filter())
+    */
+
+    constructor () {
+        this.goods = []
+    }
+
+    get totalAmount () {
+
+    }
+
+    get totalSum () {
+
+    }
+
+    clear () {
+        this.goods = [];
+    }
+
+}
+
+// Создание экземпляров товаров
+const good_1 = new Good(1, 'Брюки', 'Брюки мужские', [23, 24, ], 1520, true)
+const good_2 = new Good(2, 'Брюки', 'Брюки женские', [3, 4, 5, 6, ], 2500, true)
+const good_3 = new Good(3, 'Ботинки', 'Ботинки мужские, кожанные', [41, 42, 44, 48, ], 5370, true)
+const good_4 = new Good(4, 'Туфли', 'Туфли женские, замшавые', [33, 36, 37, ], 3700, true)
+const good_5 = new Good(5, 'Костюм Adidas', 'Костюм спортивный, мужской', [34, 42, ], 10200, true)
+
+good_2.setAvailable = false;
+
+// Создание экземпляра GoodList
+const catalog = new GoodList;
+
+// Добавление товаров в список
+catalog.add(good_1);
+catalog.add(good_2);
+catalog.add(good_3);
+catalog.add(good_4);
+catalog.add(good_5);
+
+// Вывод каталога товаров
+console.log(catalog.list)
+
+// Удаление некоторых товаров из списка
+catalog.remove(1);
+catalog.remove(3);
+
+
