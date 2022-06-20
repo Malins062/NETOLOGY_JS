5aed2a1405487bc4b2a4b334ab32bb6b8f9adddc
Malins062 <6328673@gmail.com>
2022-06-20 16:00:18 +0300

    Работа над заданием 1

---
+++ Chapter_2/Task_1/countdown/task.js
@@ -0,0 +1,25 @@
+const timer = document.getElementById('timer');
+
+setTimeout(function tick() {
+    var seconds = -1;
+    if (isNaN(Number(timer.textContent))) {
+        const a = timer.textContent.split(':'); 
+        // console.log(a);
+        seconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]);     
+        seconds--;
+    } else {
+        seconds = Number(timer.textContent) - 1;
+    }
+        
+    if (seconds == -1) {
+        alert('Вы победили в конкурсе!');
+        return;
+    } else {
+        var newTime = new Date(null);
+        newTime.setSeconds(seconds);         
+        // console.log(newTime);
+        timer.textContent = newTime.toISOString().substr(11, 8);
+
+        setTimeout(tick, 1000);
+    }
+}, 1000)
