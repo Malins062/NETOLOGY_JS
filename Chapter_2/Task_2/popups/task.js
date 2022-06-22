2c62d13b74d3fd594ac7ee650854020cbdadb3ed
Malins062 <6328673@gmail.com>
2022-06-22 17:05:09 +0300

    Завершение задания 1

--- Chapter_2/Task_2/popups/task.js
+++ Chapter_2/Task_2/popups/task.js
@@ -1,8 +1,20 @@
 const window_main = document.getElementById('modal_main');
+const window_success = document.getElementById('modal_success');
+
 window_main.className += ' modal_active'
 
-const window_close = document.getElementsByClassName('modal__close');
+const window_close = document.querySelector('.modal__close');
+const button_success = document.querySelector('.show-success');
 
-window_close.onclick(() => {
+window_close.onclick = () => {
     window_main.className = 'modal';
-})
+}
+
+window_success.onclick = () => {
+    window_success.className = 'modal';
+}
+
+button_success.onclick = () => {
+    window_success.className += ' modal_active'
+    window_main.className = 'modal';
+}
