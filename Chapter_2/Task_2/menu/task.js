3f4352e754ca56ce33bbb0dd3e225450d131a933
Malins062 <6328673@gmail.com>
2022-06-27 19:00:16 +0300

    завершение работы над заданием 2

--- Chapter_2/Task_2/menu/task.js
+++ Chapter_2/Task_2/menu/task.js
@@ -1,22 +1,32 @@
-const menuItems = document.querySelectorAll('.menu__item');
+const allMenuItems = document.querySelectorAll('.menu__item');
+console.log('Все меню all_menu__item: ', allMenuItems);
 
-console.log('Поиск menu__item: ', menuItems);
+const menuItems = Array.from(allMenuItems);
+// console.log('Нужные menu__item: ', menuItems);
+
+
+let activeMenu = null;
 
 if (menuItems.length > 0) {
+
     for (let item of menuItems) {
         
-        let parentItem = item.closest('.menu_main');
-        console.log(parentItem);
+        // console.log('Обрабатываемые клики на пункты меню: ', item);
 
-        if (parentItem != null) {
-            console.log(item);
-            item.onclick = () => {
-                let subMenu = item.querySelector('.menu_sub')
-                if (subMenu != null) {
-                    subMenu.className += ' menu_active';
-                    return false;
-                } 
-            }    
-        }
+        item.onclick = () => {
+            console.log('Активное меню: ', activeMenu);
+            if (activeMenu != null) {
+                activeMenu.className = 'menu menu_sub';
+            }
+
+            let subMenu = item.querySelector('.menu_sub');
+            // console.log(subMenu)
+       
+            if (subMenu != null && subMenu !=activeMenu) {
+                subMenu.className += ' menu_active';
+                activeMenu = subMenu;
+                return false;    
+            }
+        }    
     }
 }
