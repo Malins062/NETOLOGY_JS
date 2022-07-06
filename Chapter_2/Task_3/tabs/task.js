c582a4d1d753644cccec16dc1abdc7ebf33cd1c7
Malins062 <6328673@gmail.com>
2022-07-06 11:52:00 +0300

    Завершение работы над заданием 3

--- Chapter_2/Task_3/tabs/task.js
+++ Chapter_2/Task_3/tabs/task.js
@@ -1,1 +1,19 @@
-const tabs = document.getElementsByClassName('tab');
+const tabs = Array.from(document.getElementsByClassName('tab'));
+const tabs_content = Array.from(document.getElementsByClassName('tab__content'));
+
+// console.log(tabs);
+// console.log(tabs_content);
+
+tabs.forEach((tab) => {
+    tab.addEventListener('click', () => {
+
+        // Деактивация активной вкладки
+        const tab_active = document.querySelector('.tab_active');       
+        tab_active.className = 'tab';
+        tabs_content[tabs.indexOf(tab_active)].className = 'tab__content';
+
+        // Активация новой вкладки
+        tab.className = 'tab tab_active';
+        tabs_content[tabs.indexOf(tab)].className = 'tab__content tab__content_active';
+    })
+});
