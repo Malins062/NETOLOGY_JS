6e0f645ae99ae9f431fee5551f98f9d600d47607
Malins062 <6328673@gmail.com>
2022-07-07 16:08:29 +0300

    Работа над заданием 3

--- Chapter_3/Task_1/book-reader/task.js
+++ Chapter_3/Task_1/book-reader/task.js
@@ -5,12 +5,27 @@ bookControls.forEach((bookControl) => {
     // Каждая из настроек отображения
     const controls = Array.from(bookControl.querySelectorAll('.book_control'));
 
+    const CONTROLS_SETTINGS = [
+        {
+            nameClass: 'book__control_font-size',
+            activeClass: 'font-size_active',
+        },
+        {
+            nameClass: 'book__control_color',
+            activeClass: 'color_active',
+        },
+        {
+            nameClass: 'book__control_background',
+            activeClass: 'color_active',
+        },
+    ]
+
     // Отработка каждой настройки
     controls.forEach((control) => {
         // Виды кнопок
         const buttons = Array.from(control.querySelectorAll('a'));
 
-        buttons.forEach((button) =>{
+        buttons.forEach((button) => {
             button.addEventListener('click', () => {                
                 button.classList.add('font-size');
                 return false;
