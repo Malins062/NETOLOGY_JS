d52563f50b9ffec431b696ecee05f5ebe7653ae9
Malins062 <6328673@gmail.com>
2022-07-08 17:14:54 +0300

    commit

--- Chapter_3/Task_1/book-reader/task.js
+++ Chapter_3/Task_1/book-reader/task.js
@@ -1,3 +1,6 @@
+// Экзмепляр книги
+const book = document.getElementById('book');
+
 // Количество и виды настроек отображения книги
 const bookControls = Array.from(document.querySelectorAll('.book__controls'));
 // console.log(bookControls);
@@ -10,35 +13,56 @@ bookControls.forEach((bookControl) => {
         {
             nameClass: 'book__control_font-size',
             activeClass: 'font-size_active',
+            style: 'size',
+            styleClass: 'book_fs-',
         },
         {
             nameClass: 'book__control_color',
             activeClass: 'color_active',
+            style: 'text-color',
+            styleClass: 'book_color-',
         },
         {
             nameClass: 'book__control_background',
             activeClass: 'color_active',
+            style: 'bg-color',
+            styleClass: 'book_bg-',
         },
     ]
-    // console.log(CONTROLS_SETTINGS);
 
     // Отработка каждой настройки
     controls.forEach((control, index) => {
         // Виды кнопок
-        const buttons = Array.from(control.querySelectorAll('a'));
-        console.log(buttons);
+        const buttons = control.querySelectorAll('a');
 
         buttons.forEach((button) => {
-            // console.log(button);
+
             button.addEventListener('click', (event) => {                
-                console.log(button);
 
-                // Деактивация активной кнопки
-                const buttonActive = document.querySelector('.tab_active');       
-                buttonActive.className = 'tab';
+                // Получение активной кнопки
+                const buttonToDeactivate = control.querySelector('.' + CONTROLS_SETTINGS[index].activeClass);
 
-                // Активация нажатой кнопки
-                button.classList.add(CONTROLS_SETTINGS[index].activeClass);
+                if (buttonToDeactivate != button) {
+
+                    // Деактивация старого стиля
+                    const attributeDeactivate = buttonToDeactivate.getAttribute('data-'+CONTROLS_SETTINGS[index].style)
+                    if (attributeDeactivate) {
+                        book.classList.remove(CONTROLS_SETTINGS[index].styleClass + attributeDeactivate);
+                    }
+
+                    // Деактивация активной кнопки
+                    buttonToDeactivate.classList.remove(CONTROLS_SETTINGS[index].activeClass);
+    
+                    // Активация нажатой кнопки
+                    button.classList.add(CONTROLS_SETTINGS[index].activeClass);
+                    
+                    // Активация нового стиля
+                    const attributeActivate = button.getAttribute('data-'+CONTROLS_SETTINGS[index].style)
+                    if (attributeActivate) {
+                        book.classList.add(CONTROLS_SETTINGS[index].styleClass + attributeActivate);
+                    }
+    
+                }
 
                 event.preventDefault();
             });
