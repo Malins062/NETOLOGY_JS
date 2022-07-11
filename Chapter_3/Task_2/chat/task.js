452abbe39ea58bb896e2f8fb3f9810cb43c8b632
Malins062 <6328673@gmail.com>
2022-07-11 20:35:25 +0300

    Завершение работы над заданием 1

--- Chapter_3/Task_2/chat/task.js
+++ Chapter_3/Task_2/chat/task.js
@@ -3,44 +3,35 @@ class Chat {
     Класс для ЧАТ-БОТА, инициализируется с параметром HTML-контейнера для чата
     */
 
-    constructor(container) {
+    constructor(container, secondsToSleep = 30) {
         // Основной html - элемент
         this.container = container;
 
         // Кнопка открытия окна чата
         this.chatWidgetSide = container.querySelector('.chat-widget__side');
         
+        // Контейнер всех сообщений
+        this.chatWidgetMessages = container.querySelector('.chat-widget__messages-container');
+        
         // Сообщения
         this.messages = container.querySelector( '.chat-widget__messages' );
 
         // Стока ввод сообщения пользователя
-        this.input = container.querySelector( '.chat-widget__input' );
+        this.inputMessage = container.querySelector( '.chat-widget__input' );
 
-        console.log(this.chatWidgetSide, this.messages);
+        // Количество секунд бездействия пользователя для сворачивания чат-бота
+        this.secondsToSleep = secondsToSleep * 1000;
+
+        // Таймеры бездействия пользователя
+        this.timerId = 0;
+        this.timerOutId = 0;
+
+        // Сообщения при бездействии пользователя
+        this.TEXT_FOR_SLEEP_USER = 'Я могу чем нибудь Вам еще помочь?';
+        this.TEXTOUT_FOR_SLEEP_USER = 'Закрываю чат...';
 
         // Регистрация возможных событий
         this.registerEvents();
-    }
-
-    registerEvents() {
-        /*
-        Метод регистрации всех возможных событий в чат-боте
-        */
-
-        // Регистрация клика на сайд-баре
-        this.chatWidgetSide.addEventListener('click', () => {
-            // Формирование первоночального сообщения робота
-            this.sendMessage(this.getAnswer());
-
-            // Активация окна сообщений
-            this.container.classList.add('chat-widget_active');
-        })
-    
-        // Регистрация отправки сообщения пользователем
-        this.input.addEventListener('keyup', (e) => {
-            if (e.)
-        });
-
     }
     
     sendMessage(textMessage, isClient=false) {
@@ -62,7 +53,11 @@ class Chat {
             </div>
           </div>
         `;
-      this.messages.innerHTML += html;        
+        
+        this.messages.innerHTML += html;       
+        
+        // Прокрутка вниз до последнего сообщения
+        this.chatWidgetMessages.scrollTo(0, this.chatWidgetMessages.scrollHeight);
     }
 
     getAnswer() {
@@ -86,14 +81,68 @@ class Chat {
         ];
 
         // Случайный выбор сообщения
-        index = Math.floor(Math.random() * messages.length);
-
+        const index = Math.floor(Math.random() * messages.length);
+        
         return messages[index];
     }
+
+    setTimerToSleep() {
+        // Очистка всех таймеров
+        clearTimeout(this.timerId);
+        clearTimeout(this.timerOutId);
+        
+        this.timerId = setTimeout(() => {
+            // Формирование и отправка сообщения робота при бездействии пользователя
+            this.sendMessage(this.TEXT_FOR_SLEEP_USER);
+
+            this.timerOutId = (setTimeout(() => {
+                // Формирование и отправка сообщения робота при повторном бездействии пользователя
+                this.sendMessage(this.TEXTOUT_FOR_SLEEP_USER);
+
+                // Деактивация окна сообщений
+                this.container.classList.remove('chat-widget_active');
+            }, this.secondsToSleep));
+
+        }, this.secondsToSleep);
+
+      }
     
+    registerEvents() {
+        /*
+        Метод регистрации всех возможных событий в чат-боте
+        */
+
+        let chat = this;
+
+        // Регистрация клика на сайд-баре
+        this.chatWidgetSide.addEventListener('click', () => {
+            // Формирование первоночального сообщения робота и его отправка
+            this.sendMessage(this.getAnswer());
+
+            // Установка таймера для отслеживания бездействия пользователя
+            this.setTimerToSleep();
+
+            // Активация окна сообщений
+            this.container.classList.add('chat-widget_active');
+        })
+    
+        // Регистрация отправки сообщения пользователем
+        this.inputMessage.addEventListener('keyup', function(event) {
+            if (event.key == 'Enter' && chat.inputMessage.value.trim().length > 0) {
+                // Отправка сообщения клиента
+                chat.sendMessage(chat.inputMessage.value, true);
+
+                chat.setTimerToSleep();
+                
+                // Очистка сообщения клиента для нового ввода
+                chat.inputMessage.value = '';
+
+                // Отправка ответа бота клиенту
+                chat.sendMessage(chat.getAnswer());
+            }
+        });
+
+    }
 } 
 
-new Chat(document.querySelector('.chat-widget'));
-
-// // добавит
-// messages.innerHTML += `
+new Chat(document.querySelector('.chat-widget'), 30);
