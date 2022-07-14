956bdae541070ddaef861ebee3dedf905f06d7b5
Malins062 <6328673@gmail.com>
2022-07-14 18:12:46 +0300

    Некоторые изменения

--- Chapter_3/Task_2/interests/task.js
+++ Chapter_3/Task_2/interests/task.js
@@ -49,30 +49,3 @@ addEventListener('change', e => {
         check = (check != parent) ? parent : 0;
     }
 })
-
-
-
-
-
-
-
-
-
-/* 
-closest polyfill for ie 
-
-
-if (window.Element && !Element.prototype.closest) {
-  Element.prototype.closest =
-  function(s) {
-    var matches = (this.document || this.ownerDocument).querySelectorAll(s),
-        i,
-        el = this;
-    do {
-      i = matches.length;
-      while (--i >= 0 && matches.item(i) !== el) {};
-    } while ((i < 0) && (el = el.parentElement));
-    return el;
-  };
-}
-*/
