d5c9f5d7c7164d2bd930686ddfff18d56ce9cd71
Malins062 <6328673@gmail.com>
2022-07-15 11:37:01 +0300

    Завершение работы над заданием 2

--- Chapter_3/Task_3/todo/task.js
+++ Chapter_3/Task_3/todo/task.js
@@ -1,21 +1,140 @@
-class Task {
-    constructor(descriptionTask, listForTask) {
-        this.description = descriptionTask;
-        this.listForTask = listForTask;
-        createTask();
+// Ключ данных для локального сохранения данных
+DATA_KEY = 'netologyTasks';
+
+class HandlerTasks {
+    /*
+    Класс - обработчик задач
+    */
+
+    constructor(container) {
+        // Объект формы для ввода задачи
+        this.tasksFormControl = container.querySelector('.tasks__control');
+        
+        // Объект строки ввода задачи
+        this.tasksInput = container.querySelector('.tasks__input');
+        
+        // Объект списка задач
+        this.tasksList = container.querySelector('.tasks__list');
+
+        // Список текущих задач
+        this.tasks = [];
+        this.readTasks();
+
+        // Запуск обработчика событий
+        this.registerEvents();
     }
 
-    createTask() {
+    readTasks() {
+        /*
+        Метод чтения сохраненных задач
+        */
+
+        // Подключение к localStorage
+        const data = window.localStorage;
+        // data.removeItem(DATA_KEY);
+
+        // Чтение сохраненных данных
+        const res = data.getItem(DATA_KEY);
+
+        // Обработка данных
+        const activeTasks = res ? JSON.parse(res) : [];
+
+        // Вывод списка сохраненных задач
+        activeTasks.forEach(text => this.addTask(text));
+    }
+
+    saveTasks() {
+        /*
+        Метод записи сохраненных задач
+        */
+
+        // Подключение к localStorage
+        const data = window.localStorage;
+
+        // Удаление пустых значений
+        const activeTasks = this.tasks.filter((value) => {
+            return value;
+        })
+
+        // Сохранение массива задач в localStorage
+        data.setItem(DATA_KEY, JSON.stringify(activeTasks));
+    }
+
+    registerEvents() {
+        /*
+        Метод обработчика событий на форме ввода
+        */
+
+        // Обработчик
+        let handler = this;
+
+        // Событие на подтверждение ввода новой задачи
+        this.tasksFormControl.addEventListener('submit', function(event) {
+            // Добавление задачи
+            handler.addTask(handler.tasksInput.value);
+
+            event.preventDefault();
+        });
+    }
+
+    createTaskElement(text) {
+        /*
+        Метод создания html-элемента с текстом text
+        */
+
+        // Создание родительского div-элемента
         const taskElement = document.createElement('div');
-        const taskTitleElement = document.createElement('div');
+        taskElement.className = 'task';       
         
-        taskElement.className = '';
-        hintElement.textContent = elementForHint.title;
+        // Создание текста задачи 
+        const taskTitle = `<div class="task__title">${text}</div>`
 
-        // Добавление название класса для div
-        hintElement.className = 'tooltip';
+        // Создание кнопки удаления задачи 
+        const taskRemove = '<a href="#" class="task__remove" title="Удалить задачу">&times;</a>';
 
-        // Создание html-элемента подсказки
-        // this.listForTask.insertAdjacentElement('afterEnd', hintElement);  
+        taskElement.insertAdjacentHTML('afterBegin', taskTitle);
+        taskElement.insertAdjacentHTML('beforeEnd', taskRemove);
+    
+        return taskElement;
+    }
+
+    addTask(taskDescription) {
+        /*
+        Метод добавления задачи с текстом taskDescription, в общий список дел
+        */
+
+        // Проверка на не пустой текст задачи
+        if (taskDescription.trim().length <= 0) {
+            console.log('return');
+            return;
+        }
+
+        // Создание html-элемента задачи
+        const task = this.createTaskElement(taskDescription);
+
+        // Добавление задачи в список задач
+        this.tasksList.insertAdjacentElement('beforeEnd', task);  
+        this.tasks.push(taskDescription);
+
+        // Сохрание задач в localStorage
+        this.saveTasks();
+
+        // Индекс текущей задачи
+        const index = this.tasks.length - 1;
+
+        // Добавление обработчика для удаления задачи
+        task.querySelector('.task__remove').addEventListener('click', () => {
+            // Удаление задачи
+            task.remove();
+            delete this.tasks[index];
+            
+            // Сохрание задач в localStorage
+            this.saveTasks();
+        })
+
+        // Очистка поля ввода задачи
+        this.tasksInput.value = '';
     }
 }
+
+new HandlerTasks(document.getElementById('tasks'));
