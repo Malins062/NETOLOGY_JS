4da1737110a21ffb0f764ebdf3032b9e3fa118ff
Malins062 <6328673@gmail.com>
2022-07-20 10:20:52 +0300

    Завершение работы над заданием 2

--- Chapter_4/Task_1/poll/task.js
+++ Chapter_4/Task_1/poll/task.js
@@ -7,28 +7,41 @@ class Poll {
 
         registerEvents() - обработчик событий отправки запроса на сервер
         create(data) - метод создания опроса
-        createButton(value) - метод создания элемента "кнопки"
+            data - JSON-данные опросника
+        createButton(value, index) - метод создания элемента "кнопки"
+            value - текст кнопки
+            index - индекс кнопки/ответа
+        createButton(title, value) - метод создания элемента итоговый результатов опроса
+            title - текст ответа
+            value - значение (порцент отданных голосов)
+        totalRequest() - метод создания POST-запроса на итоги опроса
     
     */
 
     constructor (container, url) {
         this.container = container;
-        this.pollTitle = this.container.querySelector('#poll__title');
-        this.pollAnswers = this.container.querySelector('#poll__answers');
+        this.pollTitle = this.container.querySelector('.poll__title');
+        this.pollAnswers = this.container.querySelector('.poll__answers');
+        this.modalWindow = this.container.querySelector('.poll__close');
         this.url = url;
 
-        // console.log(this.container, this.#listCurrencies)
         this.registerEvents();
     }
 
     registerEvents () {
-        const xhr = new XMLHttpRequest();
+        const closeButton = this.container.querySelector('.poll__close div a');
+        closeButton.addEventListener('click', () => {
+            this.modalWindow.style.visibility = 'hidden';
+            this.totalRequest();
+        });
 
-        const container = this.container;
-        
+        const xhr = new XMLHttpRequest();
         xhr.addEventListener('readystatechange', () => {
             if (xhr.readyState == xhr.DONE) {
-                // Создание опроса
+                // Id опроса
+                this.pollId = xhr.response.id;
+
+                // Создание опроса в HTML
                 this.create(xhr.response);
             }
         });
@@ -39,13 +52,13 @@ class Poll {
     }
 
     create (data) {
-        console.log(data);
+        // Текст опроса
         this.pollTitle.textContent = data.data.title;
 
-        data.data.answers.forEach(element => this.createButton(element));
+        data.data.answers.forEach((element, index) => this.createButton(element, index));
     }
 
-    createButton (value) {
+    createButton (value, index) {
         // Создание кнопки
         const button = document.createElement('button');
         button.className = 'poll__answer'; 
@@ -54,9 +67,47 @@ class Poll {
 
         // Обработчик на клик на кнопке
         button.addEventListener('click', () => {
-            alert('спасибо')
+            this.modalWindow.style.visibility = 'visible';
+            this.pollAnswer = index; 
         });
     }
+
+    createTotalAnswer (title, value) {
+        // Создание кнопки
+        const html = `<div class=poll__result>
+                <div class=poll__result_title>${title}: </div>
+                <div class=poll__result_value>${value}%</div>
+            </div>`;
+        this.pollAnswers.insertAdjacentHTML('afterEnd', html);
+    }
+
+    totalRequest () {
+        const data = this;
+
+        const xhr = new XMLHttpRequest();
+        xhr.addEventListener('readystatechange', () => {
+            if (xhr.readyState == xhr.DONE) {
+                const answers = Array.from(this.container.querySelectorAll('.poll__answer'));
+                // console.log(this.container, answers, xhr.response);
+                answers.forEach(element => this.container.removeChild(element));
+
+                // Сумма всех голосов
+                const totalAnswers = xhr.response.stat.reduce((acc, item) => acc += item.votes, 0);
+                console.log(xhr.response.stat, totalAnswers);
+                xhr.response.stat.forEach(function (element) {
+                    // console.log(element);
+                    const percent =  (totalAnswers /  element.votes).toFixed(2);
+                    data.createTotalAnswer(element.answer, percent);
+                })
+            }
+        });
+
+        xhr.open('POST', this.url);
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        xhr.responseType = 'json';
+        xhr.send( `vote=${this.pollId}&answer=${this.pollAnswer}` );
+    }
+
 }
 
 new Poll(document.querySelector('.poll'), URL_POLL);
