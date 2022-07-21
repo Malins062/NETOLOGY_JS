175a5a58f6491c629cb1df7b8da6e5aa3b6c68e6
Malins062 <6328673@gmail.com>
2022-07-21 08:57:43 +0300

    Исправлены ошибки в задании 3

--- Chapter_4/Task_1/progressbar/task.js
+++ Chapter_4/Task_1/progressbar/task.js
@@ -7,27 +7,25 @@ document.forms.form.addEventListener('submit', (e) => {
     // console.log(progressBar, form.querySelector('#file').files[0]);
 
     const xhr = new XMLHttpRequest();
-    xhr.upload.addEventListener('onprogress', (e) => {
+    xhr.upload.onprogress = function (e) {
         if (e.lengthComputable) {
             progressBar.max = e.total;
             progressBar.value = e.loaded;
+            // console.log(e.loaded, e.total);
         }        
-        console.log(e.loaded, e.total);
-        progress.setValue(e.loaded);        
-    })
+    }
 
-    xhr.upload.addEventListener('onloadstart', (e) => {
-        console.log(e.loaded, e.total);
+    xhr.upload.onloadstart = function (e) {
+        // console.log(e.loaded, e.total);
         progressBar.value = 0;
-    });
+    }
 
-    xhr.upload.addEventListener('onloadend', (e) => {
-        console.log(e.loaded, e.total);
+    xhr.upload.onloadend = function (e) {
+        // console.log(e.loaded, e.total);
         progressBar.value = e.loaded;
-    });
+    }
 
     const dataToSend = new FormData(form);
-    dataToSend.append('file', form.querySelector('#file').files[0]);
 
     xhr.open('POST', form.getAttribute('action'), true);
     xhr.setRequestHeader('Content-Type', 'multipart/form-data');
