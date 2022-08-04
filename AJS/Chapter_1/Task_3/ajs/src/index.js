cc57d90e09cc16bdc77157a9f4b39c2f2a724fc4
Malins062 <6328673@gmail.com>
2022-08-04 14:01:04 +0300

    Работа над заданием 2

--- AJS/Chapter_1/Task_3/ajs/src/index.js
+++ AJS/Chapter_1/Task_3/ajs/src/index.js
@@ -1,10 +1,13 @@
 function info() {
     const m = new Map();
-    m.set('name', 'ajs_packet');
-    m.set('version', '1.0.0');
+    m.set('name', 'ajs');
+    m.set('version', '1.0.3');
+    m.set('description', 'Тестовый пакет JavaScript');
     return m;
 }
   
+// console.log(info());
+
 export default {
     info
-}
+};
--- AJS/Chapter_1/Task_3/ajs/webpack.config.js
+++ AJS/Chapter_1/Task_3/ajs/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'), // каталог для результатов сборки 
     filename: 'index.js', // имя файла с результатами сборки (должно совпадать с entry point в шаге 1)
-    library: 'ajs_packet', // название нашей библиотеки
+    library: 'ajs', // название нашей библиотеки
     libraryTarget: 'umd', // UMD (Universal Module Definition https://github.com/umdjs/umd) - шаблон, который позволяет использовать RequireJS и браузер
     libraryExport: 'default', // экспортируется имя default
     globalObject: 'this', // что принимать за глобальный объект, иначе сгенерируется window, а его, как вы знаете, на платформе Node.js нет
