cc752944e3322054620738071d9c3aab73b8bff9
Malins062 <6328673@gmail.com>
2022-08-12 15:12:03 +0300

    Класс character завершен

---
+++ AJS/Chapter_2/Task_1/classes/src/js/__tests__/character.test.js
@@ -0,0 +1,31 @@
+import Character from '../character';
+
+test.each([
+  [
+    ['Alex', 'Bowerman', 100, 1, 25, 25], 
+    {'name': 'Alex', 'type': 'Bowerman', 'health': 100, 'level': 1, 'attack': 25, 'defence': 25}
+  ],
+  [
+    ['Nike', 'Undead', 80, 2, 65, 15], 
+    {'name': 'Nike', 'type': 'Undead', 'health': 80, 'level': 2, 'attack': 65, 'defence': 15}
+  ],
+  [
+    ['Mike', 'Zombie', 10, 3, 15, 100], 
+    {'name': 'Mike', 'type': 'Zombie', 'health': 10, 'level': 3, 'attack': 15, 'defence': 100}
+  ],
+  [
+    ['', 'Zombie', 10, 3, 15, 100], 
+    {'name': '', 'type': 'Zombie', 'health': 10, 'level': 3, 'attack': 15, 'defence': 100}
+  ],
+  [
+    ['John', 'Spiderman', 10, 3, 15, 100], 
+    {'name': 'John', 'type': 'Spiderman', 'health': 10, 'level': 3, 'attack': 15, 'defence': 100}
+  ],
+])(
+  ('create Character testing'),
+  (params, recieved) => {
+    const expected = new Character(...params);
+
+    expect(expected).toEqual(recieved);
+  },
+);
--- AJS/Chapter_2/Task_1/classes/src/js/character.js
+++ AJS/Chapter_2/Task_1/classes/src/js/character.js
@@ -1,4 +1,4 @@
-export const TYPES_CHARACTER = [
+const TYPES_CHARACTER = [
   'Bowerman',
   'Swordsman',
   'Magician',
@@ -7,25 +7,28 @@ export const TYPES_CHARACTER = [
   'Zombie',
 ];
 
+const ERROR_NAME = 'Ошибка! Допустимая длина: 2-10 символов.';
+const ERROR_TYPE = 'Ошибка! Недопустимый тип персонажа.';
+
 /*
     Класс Character (персонаж).
     Свойства,класса Character:
-        name - имя (допустимая длина - 2-20 символов);
+        name - имя (допустимая длина: 2-10 символов);
         type - тип (допустимые значения:  TYPES_CHARACTER);
         health - уровень жизни;
         level - уровень персонажа;
         attack - атака;
         defence - защита;
 */
-export default class Character {
+class Character {
   constructor(name, type, health, level, attack, defence) {
     if (name === undefined || name.length < 2 || name.length > 10) {
-      throw new Error(`Ошибка! Персонаж ${name}: длина имени 2-10 символов.`);
+      throw new Error(ERROR_NAME);
     }
     this.name = name;
 
-    if (!(type in TYPES_CHARACTER)) {
-      throw new Error(`Ошибка! Тип пероснажа ${type}: допустимые значения ${TYPES_CHARACTER}.`);
+    if (TYPES_CHARACTER.indexOf(type) === -1) {
+      throw new Error(ERROR_TYPE);
     }
     this.type = type;
 
@@ -35,3 +38,7 @@ export default class Character {
     this.defence = defence;
   }
 }
+
+export {
+  Character as default, TYPES_CHARACTER, ERROR_NAME, ERROR_TYPE,
+};
