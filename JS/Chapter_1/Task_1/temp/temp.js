1b304bae9174f7b7c264b3e62f1d1576fd730827
Malins062 <6328673@gmail.com>
2022-10-04 12:40:56 +0300

    use moment.js

--- JS/Chapter_1/Task_1/temp/temp.js
+++ JS/Chapter_1/Task_1/temp/temp.js
@@ -1,17 +1,17 @@
-import moment from moment;
+const moment = require('./moment');
 
 //Data
 let x = {
     slotInterval: 30,
     openTime: '21:00',
-    closeTime: '05:00'
+    closeTime: '21:00'
   };
   
   //Format the time
   let startTime = moment(x.openTime, "HH:mm");
   
   //Format the end time and the next day to it 
-  let endTime = moment(x.closeTime, "HH:mm").add(1, 'days');
+  let endTime = moment(x.closeTime, "HH:mm").add(10, 'days');
   
   //Times
   let allTimes = [];
@@ -19,7 +19,7 @@ let x = {
   //Loop over the times - only pushes time with 30 minutes interval
   while (startTime < endTime) {
     //Push times
-    allTimes.push(startTime.format("HH:mm")); 
+    allTimes.push(startTime.format("DD:MM:YYYY HH:mm")); 
     //Add interval of 30 minutes
     startTime.add(x.slotInterval, 'minutes');
   }
